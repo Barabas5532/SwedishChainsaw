becc5f57719b703e957adf1d5a370a71e1fd2177
Barabas Raffai <barabas.raffai@gmail.com>
2019-08-31 11:29:15 +0100

    Added GPL3 license

--- Source/FMVDesign.h
+++ Source/FMVDesign.h
@@ -1,7 +1,24 @@
 /* Discretised FMV tonestack based on the paper "DISCRETIZATION OF
  * THE ’59 FENDER BASSMAN TONE STACK" by David T. Yeh and Julius
  * O. Smith
+ *
+ *  This file is part of Swedish Chainsaw.
+ *  Copyright (C) 2019  Barabas Raffai
+ *
+ *  Swedish Chainsaw is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  Swedish Chanisaw is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Swedish Chanisaw.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 #pragma once
 
 #include "../JuceLibraryCode/JuceHeader.h"
--- Source/HM2Filter.h
+++ Source/HM2Filter.h
@@ -1,4 +1,20 @@
 /* Simulates the Graphic EQ circuit of the HM-2 with all knobs at max settings
+ *
+ *  This file is part of Swedish Chainsaw.
+ *  Copyright (C) 2019  Barabas Raffai
+ *
+ *  Swedish Chainsaw is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  Swedish Chanisaw is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Swedish Chanisaw.  If not, see <https://www.gnu.org/licenses/>.
  */
 #pragma once
 
